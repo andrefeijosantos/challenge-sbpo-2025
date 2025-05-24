147920780582bd76214ed3a69f8e506d4f173b65
andrefeijosantos <andre.santos1@ufv.br>
2025-05-24 16:21:41 -0300

    running batches refactored

--- run_all.bat
+++
@@ -1,9 +0,0 @@
-run_A_par-it.bat
-run_A_param-alg.bat
-run_A_ref-lin.bat
-
-run_B_par-it.bat
-run_B_param-alg.bat
-run_B_ref-lin.bat
-
-PAUSE
--- run_param-alg.bat
+++
@@ -1,6 +0,0 @@
-set arg1=%1
-set arg2=%2
-set arg3=%3
-set arg4=%4
-
-java -Djava.library.path=C:/Applications/CPLEX_Studio2211/opl/bin/x64_win64 -jar target/ChallengeSBPO2025-1.0.jar datasets/%arg2%/instance_%arg1%.txt test_out.txt > logs/param-alg_%arg3%_%arg1%_%arg4%.txt
