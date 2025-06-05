9e6920f84f9c79c39a5286f6ea8e11458af4cc31
PedroFiorio <94929673+PedroFiorio@users.noreply.github.com>
2025-06-04 22:34:58 -0300

    add: matching and recombination operators

--- src/main/java/org/sbpo2025/challenge/GeneticAlgorithm.java
+++ src/main/java/org/sbpo2025/challenge/GeneticAlgorithm.java
@@ -3,12 +3,10 @@ package org.sbpo2025.challenge;
 import java.util.ArrayList;
 import java.util.BitSet;
 import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
-import java.util.PriorityQueue;
 import java.util.Random;
 import java.util.Set;
+import java.util.TreeSet;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -18,8 +16,10 @@ public class GeneticAlgorithm extends Approach {
 	
 	// Parameters
 	int _PopSize = 100;
-	int _NumIters = 100;
+	int _NumGenerations = 100;
 	double _MutationRate = 0.2;
+	double _RankPression = 1.5; // (1, 2]
+	double _EliteRate = 0.5;
 	
 	// Random numbers settings
 	private static final long SEED = 1L;
@@ -28,20 +28,21 @@ public class GeneticAlgorithm extends Approach {
 	int _MinAisles, _MaxAisles;
 	
 	Set<Individual> _Population;
+	List<Individual> _Parents, _Offspring;
 	int _IndIdControl = 0;
 	
 	public GeneticAlgorithm(Instance inst, StopWatch stopWatch, long time_limit, int minAisles, int maxAisles) {
 		super(inst, stopWatch, time_limit);
 		_MinAisles = minAisles;
 		_MaxAisles = maxAisles;
-		_Population = new HashSet<>();
+		_Population = new TreeSet<>();
 	}
 	
 	public void optimize() {
 		
 		generateInitialPop();
 	
-		for (int iter = 1; iter < _NumIters; iter++) {
+		for (int gen = 1; gen < _NumGenerations; gen++) {
 			matching();
 			recombination();
 			adaptation();
@@ -67,9 +68,74 @@ public class GeneticAlgorithm extends Approach {
 		
 	}
 	
-	private void matching() {}
+	private void matching() {
+		
+		// Ranking selection
+		List<Double> cumProb = new ArrayList<>(_PopSize);
+		List<Individual> popList = new ArrayList<>(_PopSize);
+		double baseline = (2 - _RankPression) / _PopSize;
+		double denominator = _PopSize * (_PopSize - 1);
+		
+		int i = 0;
+		for (Individual ind : _Population) {
+			popList.add(ind);
+			double p = baseline + (2 * i * (_RankPression - 1))/denominator;
+			if (i == 0) {
+				cumProb.add(p);
+				i++;
+				continue;
+			}
+			cumProb.add(cumProb.get(i-1) + p);
+			i++;
+		}
+		
+		List<Integer> result = susRoulette(cumProb, _PopSize);
+		Collections.shuffle(result);
+		
+		_Parents = new ArrayList<Individual>(_PopSize);
+		for (int j = 0; j < result.size(); j++) {
+			_Parents.add(popList.get(result.get(j)));
+		}
+		
+	}
 	
-	private void recombination() {}
+	private void recombination() {
+		
+		_Offspring = new ArrayList<Individual>(_PopSize / 2);
+		
+		for (int i = 0; i < _Parents.size(); i += 2) {
+			
+			Individual p1 = _Parents.get(i);
+			Individual p2 = _Parents.get(i);
+			
+			if (p1._Id == p2._Id) { 
+				// Apply mutation
+				
+			} else { 
+				// Crossover
+				BitSet orPs = (BitSet) p1._Cromossome.clone();
+				orPs.or(p2._Cromossome);
+				
+				BitSet andPs = (BitSet) p1._Cromossome.clone();
+				andPs.and(p2._Cromossome);
+				
+				BitSet xorRes = (BitSet) orPs.clone();
+				xorRes.xor(andPs);
+				
+				int minCard = Math.min(p1._Cromossome.cardinality(), p2._Cromossome.cardinality());
+				int maxCard = Math.max(p1._Cromossome.cardinality(), p2._Cromossome.cardinality());
+				int sonCard = RANDOM.nextInt(minCard, maxCard + 1);
+				
+				int remove = sonCard - andPs.cardinality();
+				List<Integer> positions = getOnePositions(xorRes);
+				Collections.shuffle(positions);
+				for (int j = 0; j < remove; j++) orPs.clear(positions.get(j));
+				
+				_Offspring.add(new Individual(++_IndIdControl, orPs));	
+			}
+		}
+		
+	}
 	
 	private void adaptation() {}
 	
@@ -77,4 +143,34 @@ public class GeneticAlgorithm extends Approach {
 	
 	private void selection() {}
 	
+	// ===== Auxiliary methods =====
+	
+	private List<Integer> susRoulette(List<Double> cumProb, int numSelections) {
+		List<Integer> result = new ArrayList<Integer>(numSelections);
+		int currSel = 0, i = 0;
+		double r = RANDOM.nextDouble(1.0 / numSelections);
+		while (currSel < numSelections) {
+			while (r <= cumProb.get(i)) {
+				result.set(currSel, i);
+				r += 1.0 / numSelections; 
+				currSel++;
+				
+				if(currSel == numSelections) break;
+			}
+			i++;
+		}
+		
+		return result;
+	}
+	
+	private List<Integer> getOnePositions(BitSet bitset) {
+        List<Integer> positions = new ArrayList<>(bitset.cardinality());
+        int i = bitset.nextSetBit(0);
+        while (i != -1) {
+            positions.add(i);
+            i = bitset.nextSetBit(i + 1);
+        }
+        return positions;
+    }
+		
 }
--- src/main/java/org/sbpo2025/challenge/Individual.java
+++ src/main/java/org/sbpo2025/challenge/Individual.java
@@ -2,25 +2,36 @@ package org.sbpo2025.challenge;
 
 import java.util.BitSet;
 import java.util.Objects;
+import java.util.Random;
 
-public class Individual {
+public class Individual implements Comparable<Individual> {
 
 	int _Id;
 	BitSet _Cromossome;
 	double _Fitness;
 	
+	// Temporary
+	private static final long SEED = 1L;
+    private static final Random RANDOM = new Random(SEED);
+	
 	public Individual(int id, BitSet cromossome) {
 		_Id = id;
-		_Cromossome = cromossome;
+		_Cromossome = (BitSet) cromossome.clone();
 		_Fitness = calcFitness();
 	}
 	
 	private double calcFitness() {
-		return 0.0;
+		return RANDOM.nextDouble() * 100;
 	}
 	
-	public double getFitness() {
-		return _Fitness;
+	public int getId() {
+		return _Id;
+	}
+	
+	@Override
+	public int compareTo(Individual other) {
+	    int result = Double.compare(this._Fitness, other._Fitness);
+	    return result != 0 ? result : Integer.compare(this._Id, other._Id);
 	}
 	
 	@Override
